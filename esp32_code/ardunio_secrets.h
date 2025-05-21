693f0982f218abb922909abb253d070d09ec2c36
beyza <beyzanur.yagmur@gmail.com>
2025-05-21 12:36:35 +0300

    .

--- esp32_code/ardunio_secrets.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARDUINO_SECRETS_H
-#define ARDUINO_SECRETS_H
-
-#define SECRET_SSID "Superbox53"
-#define SECRET_PASSWORD "SuperB0x.53"
-#define SECRET_SERVER_NAME "http://kemalasliyuksek.com/greenbyte/api/update_sensor_data.php"
-
-#endif
